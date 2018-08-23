5225cde79f7984350c185c570ba9345fb6548926
stephendoyin <stevedoyin@gmail.com>
2018-08-23 23:46:18 +0100

    feat: timing, fixing of bugs in js file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-/*
- * Create a list that holds all of your cards
- */
+//list that holds all of your cards
 let listOfCards = [
     "fa-diamond",
     "fa-paper-plane-o",
@@ -23,41 +21,71 @@ let listOfCards = [
 let openCards = [];
 let matchedCards = [];
 let moveCount = 0;
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-listOfCards = shuffle(listOfCards);
-fragment = document.createDocumentFragment();
+let time = 0;
+let timeIndex = 0;
+let timing;
 
-for (var x = 0; x < listOfCards.length; x++) {
-    console.log(listOfCards[x]);
-    let li = document.createElement("LI");
-    attr1 = document.createAttribute("class");
-    attr1.value = "card";
-    li.setAttributeNode(attr1);
-    let i = document.createElement("I");
-    attr3 = document.createAttribute("class");
-    attr3.value = "fa";
-    i.setAttributeNode(attr3);
-    i.classList.add(listOfCards[x]);
-    li.appendChild(i);
-    fragment.appendChild(li);
+
+//create and add card html
+function createCardHtml(){
+
+    listOfCards = shuffle(listOfCards);
+    let fragment = document.createDocumentFragment();
+    
+    for (var x = 0; x < listOfCards.length; x++) {
+        console.log(listOfCards[x]);
+        let li = document.createElement("LI");
+        attr1 = document.createAttribute("class");
+        attr1.value = "card";
+        li.setAttributeNode(attr1);
+        let i = document.createElement("I");
+        attr3 = document.createAttribute("class");
+        attr3.value = "fa";
+        i.setAttributeNode(attr3);
+        i.classList.add(listOfCards[x]);
+        li.appendChild(i);
+        fragment.appendChild(li);
+    }
+    
+    document.querySelector(".deck").appendChild(fragment);
+
 }
 
-document.querySelector(".deck").appendChild(fragment);
+createCardHtml();
+
 
 //restart game when restart icon is clicked
 const restart = document.querySelector(".restart");
-restart.addEventListener("click", function(){
+restart.addEventListener("click", reset)
+
+
+//reset game
+function reset(){
+    let cards = document.querySelectorAll(".card");
+    for(card of cards){
+        card.remove();
+    }
+    openCards = [];
+    matchedCards = [];
+    moveCount = 0;
+    time = 0;
+    timeIndex = 0;
+    createCardHtml();
+    clearInterval(timing);
+    document.querySelector(".timer").innerHTML = " 00:00";
+    document.querySelector(".moves").innerHTML = moveCount;
+}
+//reload the browser when play again is clicked
+function reload(){
+
     window.location.reload();
-});
+
+}
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
+
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -85,19 +113,30 @@ function shuffle(array) {
 
 //display card symbol when called
 function displayCardSymbol(e) {
+    if(timeIndex === 0){
+        ++timeIndex; 
+        startTime();
+    }
     if (e.target && e.target.matches("li") && (!(e.target.classList.contains("match")) && (!(e.target.classList.contains("show")))))  {
         e.target.classList.add("show");
         openCard(e);
     }
+
 }
 
-
+//events
+let modal = document.querySelector(".modal");
 let ul = document.querySelector(".deck");
 ul.addEventListener("click", displayCardSymbol, false);
+let closeButton = document.querySelector(".close-button")
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", reload);
 
 //fuctionality for opening cards
 function openCard(e) {
-    moveCounter();
+
     let target = e.target
     const cardName = target.children[0].classList[1];
     target.classList.add("open");
@@ -113,54 +152,125 @@ function openCard(e) {
     
 }
 
-
 //to locked matched card 
 function lockedPositionOnMatch(_cardName) {
+
     const cardClass = getClass(_cardName);
     let chosenCards = document.querySelectorAll(cardClass);
     for(chosenCard of chosenCards){
         chosenCard.parentNode.setAttribute("class", "card")
-        chosenCard.parentNode.classList.add("match");
+        chosenCard.parentNode.classList.add("match", "match-anim");
         matchedCards.push(_cardName);
-        console.log(chosenCard.parentNode);
     }
+
     openCards = [];
+    moveCounter();
     checkedMactched();
-    console.log(matchedCards);
 
 }
-
 
 //return class selector format 
 function getClass(_cardName) {
-    return "." + _cardName;
-}
 
+    return "." + _cardName;
+
+}
 
 //hide card symbol on chosen cards
 function removeFromOpen() {
+
     const openedCards = document.querySelectorAll(".open");
     for(let j = 0; j < 2; j++){
         
         setTimeout(function(){
             openedCards[j].classList.add("miss-match");
-        }, 200)
+            openedCards[j].classList.add("miss-anim");
+        }, 200);
         setTimeout(function(){
             openedCards[j].setAttribute("class", "card");
-        }, 700)
+        }, 700);
     }
-    openCards = []
+    openCards = [];
+    moveCounter();
+
 }
 
 //increment move and add to diplay
 function moveCounter (){ 
+    
     document.querySelector(".moves").innerHTML = ++moveCount;
+
 }
 
 //show winner board when cards have matched
 function checkedMactched(){
+
     if (matchedCards.length === listOfCards.length){
-        console.log("you have won at " + moveCount + " moves")
+        let modalText = document.querySelector(".modal-text");
+        modalText.innerHTML = `
+            <strong>Congratulations! You Won!</strong>
+            <br/>
+            <br/>with ${moveCount} moves and 1 Stars
+            <br/>
+            <strong>Time</strong>: ${getMinAndSecs(time)}
+            <br/> Wooooow!`;
+
+        setTimeout(toggleModal, 1000);
+        clearInterval(timing);
+    }
+
+    
+
+}
+
+//toggle modal
+function toggleModal(event) {
+
+    modal.classList.toggle("show-modal");
+
+}
+
+//close modal by click window area if modal is showing
+function windowOnClick(event) {
+    if (event.target === modal) {
+      toggleModal();
     }
 }
 
+//calls timer every 1 second
+function startTime(){
+    timing = setInterval(timer, 1000);
+}
+
+//incrementing every one second
+function timer() {
+    time++;
+    let li = document.querySelector(".timer");
+    li.innerHTML = getMinAndSecs(time);
+}
+
+//format time
+function getMinAndSecs(time) {
+
+    let seconds = time % 60;
+    let minutes = Math.floor(time / 60);
+    if (seconds < 10) {
+        seconds = "0"+seconds;
+    }
+    if (minutes < 1){
+        minutes = "00";
+    } else if (minutes < 10){
+        minutes = "0"+minutes
+    }
+
+    return ` ${minutes}:${seconds}`;
+
+}
+
+
+//star rating 
+function starRating(){
+
+    
+
+}
